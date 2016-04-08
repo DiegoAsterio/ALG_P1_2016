5e54e7383b570c4fb5147885e2516d0af2e41214
nacheteam <nacheteam1996@gmail.com>
2016-04-08 09:10:46 +0200

    Añadidas nuevas carpetas y datos

--- Practica 2/script_eficiencia.sh
+++ Practica 2/script_eficiencia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 g++ -std=c++11 ./Algoritmos/ejercicio6_nacho.cpp -o ./bin/ejercicio6_nacho
 rm -f ./data/datos_algoritmo_nacho.dat
-for (( i = 0; i < 10000000; i+=10000 ))
+for (( i = 0; i < 3000000; i+=10000 ))
 do
   echo -e "Iteración número ---> $i"
   ./bin/ejercicio6_nacho $i >> ./data/datos_algoritmo_nacho.dat
