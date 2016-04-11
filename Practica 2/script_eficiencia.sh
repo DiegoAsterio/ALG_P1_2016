b442fb57e5146cbefbca4983250d43b9c1fe36f5
Ignacio Aguilera Martos <nacheteam@gmail.com>
2016-04-11 20:08:50 +0200

    A

--- Practica 2/script_eficiencia.sh
+++ Practica 2/script_eficiencia.sh
@@ -8,4 +8,12 @@ do
   ./bin/unimodal_solucion1 $i >> ./data/$1/datos_solucion1_$1.dat
 done
 
+g++ -std=c++11 ./Algoritmos/opcional_bruto.cpp -o ./bin/opcional_bruto
+rm -f ./data/$1/datos_opcional_bruto_$1.dat
+for (( i = 0; i < 3000000; i+=10000 ))
+do
+  echo -e "Iteración número ---> $i"
+  ./bin/opcional_bruto $i >> ./data/$1/opcional_bruto_$1.dat
+done
+
 gnuplot ./grafica_$1.gp
