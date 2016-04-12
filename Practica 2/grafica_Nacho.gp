be4119f85a2e7cb2f43be4d5e85a58f6ceaf974b
Ignacio Aguilera Martos <nacheteam@gmail.com>
2016-04-12 18:47:50 +0200

    A

--- Practica 2/grafica_Nacho.gp
+++ Practica 2/grafica_Nacho.gp
@@ -30,3 +30,18 @@ set term png
 set output "./grphx/Nacho/opcional_bruto_Nacho.png"
 replot
 set term x11
+
+lineal(x) = l0*x + l1
+nlogaritmica(x) = n0*x*log(n1*x)
+logaritmica(x) = t0*log(t1*x) + t2
+
+fit lineal(x) "./data/Nacho/datos_genera_unimodal_secuencial_Nacho.dat" via l0,l1
+fit nlogaritmica(x) "./data/Nacho/datos_genera_unimodal_secuencial_Nacho.dat" via n0,n1
+fit logaritmica(x) "./data/Nacho/datos_genera_unimodal_secuencial_Nacho.dat" via t0,t1,t2
+
+plot "./data/Luis/datos_genera_unimodal_secuencial_Nacho.dat", lineal(x), nlogaritmica(x), logaritmica(x)
+
+set term png
+set output "./grphx/Nacho/genera_unimodal_secuencial_Nacho.png"
+replot
+set term x11
--- Practica 2/script_eficiencia.sh
+++ Practica 2/script_eficiencia.sh
@@ -10,15 +10,6 @@ do
   ./bin/unimodal_solucion1 $i >> ./data/$1/datos_solucion1_$1.dat
 done
 
-g++ -std=c++11 ./Algoritmos/Parts.cpp -o ./bin/Parts
-rm -f ./data/$1/datos_Parts_$1.dat
-for (( i = 1; i <= 3000001; i+=10000 ))
-do
-  echo -e "Iteración número ---> $i"
-  ./bin/Parts $i >> ./data/$1/datos_Parts_$1.dat
-done
-
-
 g++ -std=c++11 ./Algoritmos/opcional_bruto.cpp -o ./bin/opcional_bruto
 rm -f ./data/$1/datos_opcional_bruto_$1.dat
 for (( i = 1; i <= 15001; i+=100 ))
@@ -33,6 +24,14 @@ for (( i = 1; i <= 1000001; i+=5000 ))
 do
   echo -e "Iteración número ---> $i"
   ./bin/genera_unimodal_secuencial $i >> ./data/$1/datos_genera_unimodal_secuencial_$1.dat
+done
+
+g++ -std=c++11 ./Algoritmos/Parts.cpp -o ./bin/Parts
+rm -f ./data/$1/datos_Parts_$1.dat
+for (( i = 1; i <= 1000001; i+=5000 ))
+do
+  echo -e "Iteración número ---> $i"
+  ./bin/Parts $i >> ./data/$1/datos_Parts_$1.dat
 done
 
 gnuplot ./grafica_$1.gp
