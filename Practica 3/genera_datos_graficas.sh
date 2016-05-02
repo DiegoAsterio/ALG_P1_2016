18146f3bca92fb4139798570dd35ce42ad92a1f2
DiegoAsterio <diegoasterio.z@gmail.com>
2016-05-02 22:04:00 +0200

    Ay mai

--- Practica 3/genera_datos_graficas.sh
+++ Practica 3/genera_datos_graficas.sh
@@ -16,12 +16,12 @@ do
   ./bin/solucion_greedy_optima $i >> ./data/$1/solucion_greedy_optima_$1.dat
 done
 
-g++ -std=c++11 -w ./Algoritmos/solucion_greedy_primera.cpp -o ./bin/solucion_greedy_primera
-rm -f ./data/$1/solucion_greedy_primera$1.dat
-echo -e "Ejecutando la solucion greedy primera"
-sleep 2
-for (( i = 1; i <= 10001; i+=100 ))
-do
-  echo -e "Iteración número ---> $i"
-  ./bin/solucion_greedy_primera $i >> ./data/$1/solucion_greedy_primera$1.dat
-done
+#g++ -std=c++11 -w ./Algoritmos/solucion_greedy_primera.cpp -o ./bin/solucion_greedy_primera
+#rm -f ./data/$1/solucion_greedy_primera$1.dat
+#echo -e "Ejecutando la solucion greedy primera"
+#sleep 2
+#for (( i = 1; i <= 10001; i+=100 ))
+#do
+#  echo -e "Iteración número ---> $i"
+#  ./bin/solucion_greedy_primera $i >> ./data/$1/solucion_greedy_primera$1.dat
+#done
