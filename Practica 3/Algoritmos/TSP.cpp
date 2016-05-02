4dc409e8abc2703be18f58d4acbd59922afaabbf
DiegoAsterio <diegoasterio.z@gmail.com>
2016-05-02 23:16:46 +0200

    Ay mai

--- Practica 3/Algoritmos/TSP.cpp
+++ Practica 3/Algoritmos/TSP.cpp
@@ -90,16 +90,80 @@ TSP::void TSP_vecino_mas_cercano(vector<City>& solucion)
   return solucion;
 }
 
-TSP::void TSP_triangulillos(vector<City>& solucion){
-    list<City> solulista;
+TSP::void TSP_triangles(vector<City>& solucion){
+  list<City> solulista;
 
-    list<City> candidatos(ciudades);
+  list<City> candidatos(ciudades);
 
-    list::iterator it = candidatos.begin();
-    for (size_t i = 0; i < c; i++) {
-      if (minb.coord_x>candidatos[i].coord_x)
-        minb = candidatos[i];
+  list<City>::iterator minb = candidatos.begin();
+  for (list<City>::iterator it = candidatos.begin(); it != candidatos.end(); it++) {
+    if (*minb.coord_x>*it.coord_x)
+      minb = it;
+  }
+
+  solulista.push_back(*minb);
+  candidatos.erase(minb);
+
+  list<City>::iterator maxb = candidatos.begin();
+  for (list<City>::iterator it = candidatos.begin(); it != candidatos.end(); it++) {
+    if (*maxb.coord_x<*it.coord_x)
+      maxb = it;
+  }
+
+  solulista.push_back(*maxb);
+  candidatos.erase(maxb);
+
+
+  list<City>::iterator maxh = candidatos.begin();
+  for (list<City>::iterator it = candidatos.begin(); it != candidatos.end(); it++) {
+    if (*maxh.coord_x<*it.coord_x)
+      maxh = it;
+  }
+
+  solulista.push_back(*maxh);
+  candidatos.erase(maxh);
+
+  while (!candidatos.empty()){
+    list<City>::iterator mayor_lado = find_max_edge(solulista);
+    list<City>::iterator nearest = find_nearest_point(solulista,mayor_lado,candidatos);
+    solulista.insert(mayor_lado,*nearest);
+    candidatos.erase(nearest);
+  }
+}
+
+list<City>::iterator find_max_edge(list<City> l){
+  list<City>::iterator ret = l.begin();
+  for (list<City>::iterator it = l.begin();it != l.end();++it) {
+    if (it+1!=l.end()){
+      if(distancia(*ret.coord_x,*ret.coord_y,*(ret+1).coord_x,*(ret+1).coord_y)
+          < distancia(*it.coord_x,*it.coord_y,*(it+1).coord_x,*(it+1).coord_y))
+        ret = it;
+    }else{
+      if (distancia(*ret.coord_x,*ret.coord_y,*(ret+1).coord_x,*(ret+1).coord_y)
+          < distancia(*it.coord_x,*it.coord_y,l.front().coord_x,l.front().coord_y))
+        ret = it;
     }
+  }
+  return ret;
+}
 
+list<City>::iterator find_nearest_point(list<City> orig, list<City>::iterator it, list<City> searching) {
+  City city1,city2;
+  if (it+1 == it.end()) {
+    city1 = *it;
+    city2 = orig.front()
+  }
+  else{
+    city1=*it;
+    city2=*(it+1);
+  }
 
+  list<City>::iterator ret = searching.begin();
+
+  for (list<City>::iterator it = searching.begin(); it != searching.end(); ++it) {
+    if(distancia(city1.coord_x,*it.coord_x,city1.coord_y,*it.coord_y) + distancia(city2.coord_x,*it.coord_x,city2.coord_y,*it.coord_y)
+      < distancia(city1.coord_x,*ret.coord_x,city1.coord_y,*ret.coord_y) + distancia(city2.coord_x,*ret.coord_x,city2.coord_y,*ret.coord_y))
+      ret = it;
+  }
+  return ret;
 }
