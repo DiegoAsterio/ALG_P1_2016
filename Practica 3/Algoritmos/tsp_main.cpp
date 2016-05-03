2c409950591cfd9a444fef2d180ee622961b3cac
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2016-05-03 18:30:09 +0200

    A

--- Practica 3/Algoritmos/tsp_main.cpp
+++ Practica 3/Algoritmos/tsp_main.cpp
@@ -7,24 +7,29 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  if (argc<3)
+  if (argc<2)
   {
-    cerr << "La sintaxis es <datos_input> <solucion_output>" << endl;
-    exit(1);
+    cerr << "La sintaxis es <datos_input>" << endl;
+    return(1);
   }
 
   TSP t_vecino_mas_cercano(argv[1]), t_triangles(argv[1]), t_random_swap(argv[1]);
+
   vector<City> v_vecino_mas_cercano, v_triangles, v_random_swap;
-  t_vecino_mas_cercano.TSP_vecino_mas_cercano(v_vecino_mas_cercano);
-  t_triangles.TSP_triangles(v_triangles);
+
+  //t_vecino_mas_cercano.TSP_vecino_mas_cercano(v_vecino_mas_cercano);
+  //t_triangles.TSP_triangles(v_triangles);
   t_random_swap.TSP_RandomSwap(50,v_random_swap);
+
   string output(argv[2]);
-  ofstream f_vecino_mas_cercano(output+"_vecino_mas_cercano.tsp");
-  ofstream f_triangles(output+"_triangles.tsp");
-  ofstream f_random_swap(output+"_random_swap.tsp");
+
+  ofstream f_vecino_mas_cercano("sol_vecino_mas_cercano.tsp");
+  ofstream f_triangles("sol_triangles.tsp");
+  ofstream f_random_swap("sol_random_swap.tsp");
+
   t_vecino_mas_cercano.TSP_WriteBack(f_vecino_mas_cercano, v_vecino_mas_cercano);
   t_triangles.TSP_WriteBack(f_triangles, v_triangles);
   t_random_swap.TSP_WriteBack(f_random_swap, v_random_swap);
 
-  exit(0);
+  return(0);
 }
