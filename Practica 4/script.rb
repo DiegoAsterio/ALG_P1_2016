008eb1643c0d24c2f6bb429d0ce5e86c97527143
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2016-05-29 00:49:27 +0200

    A

---
+++ Practica 4/script.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+system( "g++ -std=c++11 ./src/cena_gala_nacho.cpp -o ./bin/cena_gala_nacho" )
+puts "Ejecutando cena_gala_nacho..."
+(1..250).each do |x|
+  if(x%50==0)
+    puts "Iteracion: " + x.to_s
+  end
+  system( "./bin/cena_gala_nacho " + x.to_s + " >> ./data/cena_gala_nacho_" + ARGV[0].to_s + ".dat")
+end
