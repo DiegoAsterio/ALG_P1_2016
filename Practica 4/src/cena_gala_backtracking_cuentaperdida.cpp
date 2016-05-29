3e572610fafc5a8926af6560e6ad2a132c562211
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2016-05-29 17:43:12 +0200

    A

--- Practica 4/src/cena_gala_backtracking_cuentaperdida.cpp
+++ Practica 4/src/cena_gala_backtracking_cuentaperdida.cpp
@@ -4,8 +4,9 @@
 #include <time.h>
 #include <stdlib.h>
 #include <iostream>
+#include <chrono>
 using namespace std;
-
+using namespace std::chrono;
 //Cuenta la afinidad que ya has perdido cogiendo la distribución por ahora hecha en rama
 int afinidadNoConseguida(list<int> rama, int* afinidades, int n){
 	int res = 0;
@@ -50,31 +51,9 @@ list<int> backtracking(list<int> rama, int* afinidades, list<int> sinusar, int &
 		}
 	}
 }
-int main(int argc, char* argv[]){
-	if (argc<2)
-	{
-		cerr << "La sintaxis es <num_personas>" << endl;
-		return(1);
-	}
-	srand(time(0));
-	int n = atoi(argv[1]);
-	int* afinidades = new int[n*n];
-	list<int> rama ; 
-	list<int> sinusar ;
-	for(int i = 0; i < n; i++){		// Rellena afinidades
-		sinusar.push_back(i);
-		for(int j = i; j < n; j++){
-			afinidades[i*n+j] = afinidades[j*n+i] = rand()%101;
-			if(i == j)
-				afinidades[i*n+i] = 0;  // La afinidad contigo mismo es 0, si.
-		}
-	}
-	rama.push_back(0);   // La mesa es redonda, el primer elemento da igual el que sea.
-	sinusar.pop_front();
-	
-	int NoAfinidad = n*100;
-	rama = backtracking(rama, afinidades, sinusar, NoAfinidad, n);
-	
+
+void Imprime(int* afinidades, int n, int NoAfinidad, list<int> rama)
+{
 	for(int i = 0; i < n; i++){		// IMPRIME MATRIZ DE AFINIDADES
 		for(int j = 0; j < n; j++){
 			cout << afinidades[i*n+j] << " ";
@@ -86,5 +65,39 @@ int main(int argc, char* argv[]){
 		cout << *it << " - " ;
 	}
 	cout << endl << "La afinidad conseguida es " << 100*n - NoAfinidad << endl;   // IMPREME LA AFINIDAD CONSEGUIDA
-	
+}
+
+int main(int argc, char* argv[]){
+	if (argc<2)
+	{
+		cout << "La sintaxis es <num_personas>" << endl;
+		return(1);
+	}
+	srand(time(0));
+	int n = atoi(argv[1]);
+	int* afinidades = new int[n*n];
+	list<int> rama ;
+	list<int> sinusar ;
+	for(int i = 0; i < n; i++){		// Rellena afinidades
+		sinusar.push_back(i);
+		for(int j = i; j < n; j++){
+			afinidades[i*n+j] = afinidades[j*n+i] = rand()%101;
+			if(i == j)
+				afinidades[i*n+i] = 0;  // La afinidad contigo mismo es 0, si.
+		}
+	}
+	rama.push_back(0);   // La mesa es redonda, el primer elemento da igual el que sea.
+	sinusar.pop_front();
+
+	high_resolution_clock::time_point t_antes, t_despues;
+	int NoAfinidad = n*100;
+
+	t_antes = high_resolution_clock::now();
+	rama = backtracking(rama, afinidades, sinusar, NoAfinidad, n);
+	t_despues = high_resolution_clock::now();
+
+	Imprime(afinidades, n, NoAfinidad, rama);
+	duration<double> transcurrido = duration_cast<duration<double> >(t_despues-t_antes);
+  cout << n << " " << transcurrido.count() << "\n";
+
 }
